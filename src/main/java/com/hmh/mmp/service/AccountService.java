caefb74cec896317f1399196d22a71633955c7f0
Hwang MyungHa <audgk425@gmail.com>
2022-01-27 11:12:33 +0900

    Wait
    I will run after bug fix (macbook stop when do mirroring

--- src/main/java/com/hmh/mmp/service/AccountService.java
+++ src/main/java/com/hmh/mmp/service/AccountService.java
@@ -1,7 +1,12 @@
 package com.hmh.mmp.service;
 
+import com.hmh.mmp.dto.account.AccountDetailDTO;
 import com.hmh.mmp.dto.account.AccountSaveDTO;
+
+import java.util.List;
 
 public interface AccountService {
     Long save(AccountSaveDTO accountSaveDTO);
+
+    List<AccountDetailDTO> findAll(Long bankId);
 }
--- src/main/java/com/hmh/mmp/service/AccountServiceImpl.java
+++ src/main/java/com/hmh/mmp/service/AccountServiceImpl.java
@@ -1,9 +1,14 @@
 package com.hmh.mmp.service;
 
+import com.hmh.mmp.dto.account.AccountDetailDTO;
 import com.hmh.mmp.dto.account.AccountSaveDTO;
 import com.hmh.mmp.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,8 +18,21 @@ public class AccountServiceImpl implements AccountService{
 
     @Override
     public Long save(AccountSaveDTO accountSaveDTO) {
+        MultipartFile accountPhoto = accountSaveDTO.getAccountPhoto();
+        String accountPhotoName = accountPhoto.getOriginalFilename();
+        accountPhotoName = System.currentTimeMillis() + "-" + accountPhotoName;
 
-        
+        String savePath = "/Users/myungha/Desktop/Github/MoneyManager_for_intel/src/main/resources/photo/account" + accountPhotoName;
+        if (!accountPhoto.isEmpty()) {
+            accountPhoto.transferTo(new File(savePath));
+            accountSaveDTO.setAccountPhotoName();
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<AccountDetailDTO> findAll(Long bankId) {
         return null;
     }
 }
